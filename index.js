bfe9a203a7fd5824d066e033413d3834955449e5
valennegrelli02 <valentin.negrelli@mi.unc.edu.ar>
2023-01-20 14:35:04 -0300

    shorturl post

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const dns = require("dns");
+let psl = require("psl");
+let bodyParser = require("body-parser");
+
+//---------------AUX FUNCTIONS-------------//
+
+// function extractHostname(url) {
+//   var hostname;
+//   //find & remove protocol (http, ftp, etc.) and get hostname
+
+//   if (url.indexOf("//") > -1) {
+//     hostname = url.split("/")[2];
+//   } else {
+//     hostname = url.split("/")[0];
+//   }
+
+//   //find & remove port number
+//   hostname = hostname.split(":")[0];
+//   //find & remove "?"
+//   hostname = hostname.split("?")[0];
+
+//   return hostname;
+// }
+
+//-----------------APP-------------------//
+
 const app = express();
 
 // Basic Configuration
@@ -8,17 +34,39 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: "hello API"});
 });
 
-app.listen(port, function() {
+let ShortenedURLs = {};
+let index = 0;
+
+const urlEncodedDataMware = bodyParser.urlencoded({extended: false});
+app.use(urlEncodedDataMware);
+
+app.post("/api/shorturl", function (req, res) {
+  const url = req.body.url;
+  const hostname = new URL(url).hostname;
+  // psl.get(extractHostname(url));
+  console.log(url);
+  dns.lookup(hostname, function (err, address, family) {
+    if (!err) {
+      ShortenedURLs[url] = index;
+      res.json({original_url: url, shorturl: index});
+      index++;
+    } else {
+      res.json({error: "Invalid URL"});
+    }
+  });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
